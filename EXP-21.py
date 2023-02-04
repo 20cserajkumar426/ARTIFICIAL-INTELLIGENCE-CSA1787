cb093e5c894a6b8391a5afa63f3ed3d859efb685
20cserajkumar426 <113225771+20cserajkumar426@users.noreply.github.com>
2023-02-04 14:45:23 +0530

    Add files via upload

---
+++ EXP-21.py
@@ -0,0 +1,14 @@
+import time
+
+
+#initialize global variables
+XO = 'x'
+winner = None
+draw = False
+width = 400
+height = 400
+white = (255, 255, 255)
+line_color = (10,10,10)
+
+#TicTacToe 3x3 board
+TTT = [[None]*3,[None]*3,[None]*3]
